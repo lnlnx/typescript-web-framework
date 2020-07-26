59fd7ee1719f9974dbc662d1fb8cf3f5da579cee
Tingting <ttina.chenn@gmail.com>
2020-07-26 11:11:01 -0400

    Added basic structure of eventing and sync

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import { User } from './models/User';
 
-const user = new User({});
-user.set({ name: 'test' });
-console.log(user.get('name'));
+const user = new User({ name: 'test', age: 21 });
+
+user.on('changed', () => {
+  console.log('test changed');
+});
+
+user.on('saved', () => {
+  console.log(user);
+});
+
+user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,7 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
+import { AxiosResponse } from 'axios';
 
 export interface UserProp {
   id?: number;
@@ -10,16 +12,51 @@ export interface UserProp {
 const RootUrl = 'http://localhost:3000/users';
 
 export class User {
-  events: Eventing = new Eventing();
-  sync: Sync<UserProp> = new Sync<UserProp>(RootUrl);
+  private events: Eventing = new Eventing();
+  private sync: Sync<UserProp> = new Sync<UserProp>(RootUrl);
+  private attributes: Attributes<UserProp>;
 
-  constructor(private data: UserProp) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
+  constructor(data: UserProp) {
+    this.attributes = new Attributes<UserProp>(data);
   }
 
-  set(update: UserProp): void {
-    Object.assign(this.data, update);
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+
+  set(data: UserProp): void {
+    this.attributes.set(data);
+    this.events.trigger('changed');
+  }
+
+  fetch() {
+    const id = this.get('id');
+
+    if (typeof id !== 'number') {
+      throw new Error('Must provide user id!');
+    }
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+
+  save() {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+        this.trigger('saved');
+      })
+      .catch(() => {
+        this.trigger('error');
+      });
   }
 }
