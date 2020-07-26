a1e7601de89097e11e9d9c696c6096df4eac66b3
Tingting <ttina.chenn@gmail.com>
2020-07-26 16:43:31 -0400

    Added basic functions to insert HTML element into dom.

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,19 +1,41 @@
 export class UserForm {
   constructor(public parent: Element) {}
 
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick,
+    };
+  }
+
+  onButtonClick(): void {
+    console.log('clicked');
+  }
+
   template(): string {
     return `
       <div>
         <h1> User Input </h1>
         <input></input>
+        <button>Click me!</button>
       </div>
     `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventMap = this.eventsMap();
+
+    for (let eventKey in eventMap) {
+      const [eventName, selector] = eventKey.split(':');
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventMap[eventKey]);
+      });
+    }
   }
 
   render() {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
-
+    this.bindEvents(templateElement.content);
     this.parent.append(templateElement.content);
   }
 }
